453818ba404d967289ae56a4485006ae57b1b0da
1p22geo <1p22geodecki@gmail.com>
2023-06-28 16:50:23 +0200

    changed default redirect

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,6 @@
-import Link from 'next/link'
-import { cookies, headers } from 'next/headers'
-import { redirect } from 'next/navigation';
+"use client"
 export default async function Home() {
-  redirect("https://192.168.50.156/en")
-  
+  "use client"
+    window.location.pathname = '/en'
+    return <></>
 }
