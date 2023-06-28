3adbcdc12b1b4ddb6ef83308fe34bbca53d45557
1p22geo <1p22geodecki@gmail.com>
2023-06-28 16:14:21 +0200

    Added all the secret files.

---
+++ lib/globals/secrets/index.ts
@@ -0,0 +1,8 @@
+export default {
+    database:{
+        DB_CONN_STRING:"mongodb+srv://dbuser:A3rZbY7tLorCXZUR@cluster0.nthvuuy.mongodb.net/?retryWrites=true&w=majority"
+    },
+    application:{
+        ROOT_PASS:"P=4pierkwadrat"
+    }
+}
